77da618c2c5cfa97c0db0d1d6117cf5d4d3d163a
gsantella <gsantella@southhills.edu>
2024-12-20 12:18:01 -0500

    added some dogs and HttpClient provider for all

--- src/app/guido/c1/c1.component.ts
+++ src/app/guido/c1/c1.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { C6Component } from '../c6/c6.component';
+import { C5Component } from "../c5/c5.component";
 
 @Component({
   selector: 'app-guido-c1',
-  imports: [C6Component],
+  imports: [C6Component, C5Component],
   templateUrl: './c1.component.html',
   styleUrl: './c1.component.css'
 })
--- src/app/guido/c5/c5.component.ts
+++ src/app/guido/c5/c5.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './c5.component.css'
 })
 export class C5Component {
+
+  jsondata:any
+  dogPhotoURL:string = 'https://images.dog.ceo/breeds/vizsla/n02100583_7467.jpg'
+  
+  constructor(private httpClient: HttpClient){}
+
+  getApiData() {
+    this.httpClient.get("https://dog.ceo/api/breeds/image/random").subscribe( result => {
+      this.jsondata = result
+      this.dogPhotoURL = this.jsondata.message;
+    }) 
+  }
 
 }
