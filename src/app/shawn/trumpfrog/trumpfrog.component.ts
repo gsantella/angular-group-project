4be93518d82d3a0b29eff2eeedfcae47568851d3
Shawn Seltner <148699849+Oppslia@users.noreply.github.com>
2026-05-13 13:56:57 -0400

    TrumpFrog Refactor with signals

--- src/app/shawn/trumpfrog/trumpfrog.component.ts
+++ src/app/shawn/trumpfrog/trumpfrog.component.ts
@@ -1,33 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  WritableSignal,
+  signal,
+  Signal,
+  linkedSignal,
+} from '@angular/core';
+
+enum TrumpFrogState {
+  trump = 'Trump Mode',
+  frog = 'Frog Mode',
+}
 
 @Component({
   selector: 'app-trumpfrog',
   imports: [],
-  templateUrl: './trumpfrog.component.html',
-  styleUrl: './trumpfrog.component.css'
+  template: `
+  <h1>Trump-Frog</h1>
+  <img [src]="imageSrc()">
+  <div style="display:flex;flex-direction: row;">
+      <button 
+        style="margin-top: 15px;" 
+        (mouseover)='hoverChangeText()' 
+        (click)="changeImage()" 
+        (mouseleave)="hoverExitSwitch()">{{textSrc()}}
+      </button>
+      <h2 style="margin-left: 20px;">Look at this flawless button tech!</h2>
+  </div>
+  <p>Component data bound to HTML property</p>
+  `,
+  styleUrl: './trumpfrog.component.css',
 })
 export class TrumpfrogComponent {
-  image1:string = 'https://www.brookings.edu/wp-content/uploads/2020/11/2020-11-04T082617Z_1408284537_RC28WJ99RD0N_RTRMADP_3_USA-ELECTION-TRUMP.jpg'
-  image2:string = 'https://www.lvzoo.org/wp-content/themes/lvzoo/timthumb.php?src=https://www.lvzoo.org/wp-content/uploads/2015/11/Whites-Tree-Frog-image1.jpg&w=776&h=771'
-  text1:string = 'Trump Mode'
-  text2:string = 'Frog Mode'
-  imageSrc:string = this.image1 // default
-  buttonText:string = this.text1 // default
-  changeImage(){
-    this.imageSrc == this.image1 ? (()=>{
-      this.imageSrc = this.image2;
-      this.buttonText = this.text2
-      })() : (()=>{
-      this.imageSrc = this.image1
-      this.buttonText = this.text1
-      })()
+  private readonly state: WritableSignal<boolean> = signal<boolean>(false);
+  private readonly buttonState: WritableSignal<boolean> = linkedSignal<boolean>(
+    () => !this.state()
+  );
+  protected readonly imageSrc: Signal<string> = computed<string>(() =>
+    this.state()
+      ? 'https://i.natgeofe.com/k/8fa25ea4-6409-47fb-b3cc-4af8e0dc9616/red-eyed-tree-frog-on-leaves-3-2_2x3.jpg'
+      : 'https://www.brookings.edu/wp-content/uploads/2020/11/2020-11-04T082617Z_1408284537_RC28WJ99RD0N_RTRMADP_3_USA-ELECTION-TRUMP.jpg'
+  );
+  protected readonly textSrc: Signal<TrumpFrogState> = computed<TrumpFrogState>(
+    () => (this.buttonState() ? TrumpFrogState.trump : TrumpFrogState.frog)
+  );
+
+  protected changeImage(): void {
+    this.state.update((v: boolean) => !v);
   }
-  hoverChangeText(){
-    this.buttonText == this.text2 ? this.buttonText = this.text1 : this.buttonText = this.text2
-    
+
+  protected hoverChangeText(): void {
+    this.buttonState.update((v: boolean) => (this.state() == v ? v : !v));
   }
-  hoverExitSwitch(){
-    this.imageSrc == this.image1 ? this.buttonText = this.text1 : this.buttonText = this.text2
+
+  protected hoverExitSwitch(): void {
+    this.buttonState.update((v: boolean) => (this.state() == v ? !v : v));
   }
 }
-
